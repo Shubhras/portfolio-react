3e0a4a7f0d6c7c255d25995abcba094d9f55f111
vinaysharma <vinaykumar857461@gmail.com>
2023-05-19 12:32:14 +0530

    working design banner and card

--- src/components/RecentProjects/ProjectCard.js
+++ src/components/RecentProjects/ProjectCard.js
@@ -29,18 +29,39 @@ const ProjectCard = ({item}) => {
   </div>
   </div>
   
-  <div className="p-4 rounded-lg bg-indigo-300 dark:bg-indigo-800 dark:text-indigo-400 w-8/12 h-96 bg-neutral-200 rounded-3xl shadow-lg">03
-  <h3>UX/UI</h3>
-  <p>Cookwise</p>
+  <div className="p-4 rounded-lg bg-indigo-300 dark:bg-indigo-800 dark:text-indigo-400 w-8/12 h-96 bg-neutral-200 rounded-3xl shadow-lg">
+ 
+  <div className="-mt-5 -ml-4 -mr-4 bg-stone-800 h-60 rounded-ss-3xl rounded-tr-3xl">
+  <h3 className="p-28 font-Montserrat text-xl">UX/UI</h3>
+  </div>
+  <div className="mt-8 text-left font-mono">
+  <p className="text-black">Cookwise</p>
+  <a href="#"className="inline-block rounded-md border border-transparent bg-rose-500 px-8 py-3 text-center font-medium text-white hover:bg-rose-500 mt-8 font-mono" >
+                Download CV
+       </a>
+  </div>
   </div>
   <div className="p-4 rounded-lg bg-indigo-300 dark:bg-indigo-800 dark:text-indigo-400 w-8/12 h-96 bg-neutral-200 rounded-3xl shadow-lg">
-    04
-    <h3>Web Design</h3>
-    <p>Fredrik Nordström</p>
+    <div className="-mt-5 -ml-4 -mr-4 bg-stone-800 h-60 rounded-ss-3xl rounded-tr-3xl">
+  <h3 className="p-28 font-Montserrat text-xl">Web Design</h3>
+  </div>
+  <div className="mt-8 text-left font-mono">
+  <p className="text-black">Fredrik Nordström</p>
+  <a href="#"className="inline-block rounded-md border border-transparent bg-rose-500 px-8 py-3 text-center font-medium text-white hover:bg-rose-500 mt-8 font-mono" >
+                Download CV
+       </a>
+  </div>
     </div>
-  <div className="p-4 rounded-lg bg-indigo-300 dark:bg-indigo-800 dark:text-indigo-400 w-8/12 h-96 bg-neutral-200 rounded-3xl shadow-lg">05
-  <h3>UX/UI</h3>
-  <p>Bike</p>
+  <div className="p-4 rounded-lg bg-indigo-300 dark:bg-indigo-800 dark:text-indigo-400 w-8/12 h-96 bg-neutral-200 rounded-3xl shadow-lg">
+  <div className="-mt-5 -ml-4 -mr-4 bg-stone-800 h-60 rounded-ss-3xl rounded-tr-3xl">
+  <h3 className="p-28 font-Montserrat text-xl">UX/UI</h3>
+  </div>
+  <div className="mt-8 text-left font-mono">
+  <p className="text-black">Bike</p>
+  <a href="#"className="inline-block rounded-md border border-transparent bg-rose-500 px-8 py-3 text-center font-medium text-white hover:bg-rose-500 mt-8 font-mono" >
+                Download CV
+       </a>
+  </div>
   </div>
 </div>
 </div>
